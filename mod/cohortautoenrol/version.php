d95a4155074792efb80b47dd3f32de4cde7bc3b9
Dmitri Pisarev <dimaip@gmail.com>
2018-12-10 14:36:14 +0300

    BUGFIX: rename dummy table name

--- mod/cohortautoenrol/version.php
+++ mod/cohortautoenrol/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018121006;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2018121007;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018050800;    // Requires this Moodle version
 $plugin->component = 'mod_cohortautoenrol';        // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
