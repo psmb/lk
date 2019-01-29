145bbdae98081c485072c83eeeebc60a0745dbe4
Dmitri Pisarev <dimaip@gmail.com>
2019-01-29 18:59:32 +0300

    FEATURE: enrol with correct roleid based on cohort description

--- local/cohortautoenrol/classes/task/sync.php
+++ local/cohortautoenrol/classes/task/sync.php
@@ -21,24 +21,26 @@ class sync extends \core\task\scheduled_task {
         $DB->delete_records('enrol', array('enrol' => 'cohort'));
 
         $trace->output("Creating new cohort enrollments...");
-        $cohortSql = "SELECT course.id AS courseid, course.fullname, joined2.cohortid, joined2.cohortidnumber FROM (
-            SELECT categories.id, joined1.cohortid, joined1.cohortidnumber from (
-                SELECT context.instanceid, cohort.id AS cohortid, cohort.idnumber AS cohortidnumber FROM {cohort} AS cohort JOIN {context} as context on cohort.contextid=context.id where context.contextlevel=40
+        $cohortSql = "SELECT course.id AS courseid, course.fullname, joined2.cohortid, joined2.cohortidnumber, joined2.cohortdescription FROM (
+            SELECT categories.id, joined1.cohortid, joined1.cohortidnumber, joined1.cohortdescription from (
+                SELECT context.instanceid, cohort.id AS cohortid, cohort.idnumber AS cohortidnumber, cohort.description AS cohortdescription FROM {cohort} AS cohort JOIN {context} AS context ON cohort.contextid=context.id WHERE context.contextlevel=40
             ) AS joined1 JOIN {course_categories} AS categories ON categories.path LIKE CONCAT('%/', joined1.instanceid, '/%') OR categories.path LIKE CONCAT('%/', joined1.instanceid)
         ) AS joined2 JOIN {course} AS course ON course.category=joined2.id";
 
         $cohortRs = $DB->get_recordset_sql($cohortSql);
         foreach($cohortRs as $cohortRow) {
+            // if description contains "БЕСПЛАТНО", then assign as unrestricted user
+            $roleid = \strpos($cohortRow->cohortdescription, 'БЕСПЛАТНО') !== false ? 9 : 5;
             $record = (object) [
                 'enrol' => 'cohort',
-                'status' => '0',
+                'status' => 0,
                 'courseid' => $cohortRow->courseid,
-                'roleid' => '9',
+                'roleid' => $roleid,
                 'customint1' => $cohortRow->cohortid,
-                'customint2' => '0'
+                'customint2' => 0
             ];
             $DB->insert_record('enrol', $record);
-            $trace->output(sprintf("  creating cohort sync in course '%s' for cohort '%s'", $cohortRow->fullname, $cohortRow->cohortidnumber));
+            $trace->output(sprintf("  creating cohort sync in course '%s' for cohort '%s' with roleid '%s'", $cohortRow->fullname, $cohortRow->cohortidnumber, $roleid));
         }
 
         // Sync the cohorts now
