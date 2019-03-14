9f7475910d5deb73f5efe8494d09d69fd255e192
Dmitri Pisarev <dimaip@gmail.com>
2019-03-14 15:18:28 +0300

    TASK: make analytics more robust

--- local/cohortautoenrol/analytics.php
+++ local/cohortautoenrol/analytics.php
@@ -42,19 +42,22 @@ foreach(array_reverse($payments) as $payment) {
 
     $total += (float)$payment->Amount;
     
-    list($context, $course, $cm) = get_context_info_array($data->contextid);
-    $file = $DB->get_record_sql("SELECT * FROM {files} WHERE filesize > 0 AND contextid=$data->contextid AND component = 'mod_resource' LIMIT 1");
-    $user = $DB->get_record_sql("SELECT * FROM {user} where id = $data->userid");
-    // $context = $DB->get_record_sql("SELECT instanceid, path FROM {context} WHERE contextlevel=70 AND id=$data->contextid");
-    $path = explode('/', $context->path);
-    $topCategoryContextId = $path[3];
-    $topCategoryContext = $DB->get_record_sql("SELECT * FROM {context} WHERE contextlevel=40 AND id=$topCategoryContextId");
-    $topCategory = $DB->get_record_sql("SELECT * FROM {course_categories} WHERE id=$topCategoryContext->instanceid");
+    $file = $DB->get_record_sql("SELECT * FROM {files} WHERE filesize > 0 AND contextid=$data->contextid AND component = 'mod_resource' LIMIT 1", null, IGNORE_MISSING);
+    $fileName = $file ? $file->filename : $data->fileName;
+    $user = $DB->get_record_sql("SELECT * FROM {user} where id = $data->userid", null, IGNORE_MISSING);
+    try {
+        list($context, $course, $cm) = get_context_info_array($data->contextid);
+        $path = explode('/', $context->path);
+        $topCategoryContextId = $path[3];
+        $topCategoryContext = $DB->get_record_sql("SELECT * FROM {context} WHERE contextlevel=40 AND id=$topCategoryContextId", null, IGNORE_MISSING);
+        $topCategory = $DB->get_record_sql("SELECT * FROM {course_categories} WHERE id=$topCategoryContext->instanceid", null, IGNORE_MISSING);
+    } catch (Exception $e) {
+    }
 
     echo "<tr>
         <td><a target='_blank' href=\"https://merchant.cloudpayments.ru/transactions/$payment->TransactionId\">$payment->DateTime</a></td>
         <td>$payment->Amount</td>
-        <td>$file->filename</td>
+        <td>$fileName</td>
         <td><a href=\"/course/view.php?id=$course->id\">$course->fullname</td>
         <td>$topCategory->name</td>
         <td>$user->email</td>
