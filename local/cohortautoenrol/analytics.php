455bab199e2ee6539c4dbbb264f9edf35379fff9
root <root@server.psmb.ru>
2019-05-07 13:27:48 +0300

    FEATURE: annalytics grouping

--- local/cohortautoenrol/analytics.php
+++ local/cohortautoenrol/analytics.php
@@ -1,5 +1,20 @@
 <?php
 require_once '../../config.php';
+# Sorry. There's something wrong with locales here
+$monthNames = [
+  'Январь',
+  'Февраль',
+  'Март',
+  'Апрель',
+  'Май',
+  'Июнь',
+  'Июль',
+  'Август',
+  'Сентябрь',
+  'Октябрь',
+  'Ноябрь',
+  'Декабрь'
+];
 
 require_login();
 
@@ -21,53 +36,79 @@ $context = stream_context_create([
 
 $json = file_get_contents('https://payments.sfi.ru/es-projection/lk-payments', false, $context);
 $payments = json_decode($json);
-
-echo "<table class='generaltable'>
-    <thead>
-        <tr>
-            <th>Время платежа</th>
-            <th>Сумма, руб.</th>
-            <th>Имя файла</th>
-            <th>Курс</th>
-            <th>Подразделение</th>
-            <th>Email</th>
-            <th>Имя</th>
-        </tr>
-    </thead>";
-
-$total = 0;
-
-foreach(array_reverse($payments) as $payment) {
-    $data = json_decode($payment->Data);
-
-    $total += (float)$payment->Amount;
-    
-    $file = $DB->get_record_sql("SELECT * FROM {files} WHERE filesize > 0 AND contextid=$data->contextid AND component = 'mod_resource' LIMIT 1", null, IGNORE_MISSING);
-    $fileName = $file ? $file->filename : $data->fileName;
-    $user = $DB->get_record_sql("SELECT * FROM {user} where id = $data->userid", null, IGNORE_MISSING);
-    try {
-        list($context, $course, $cm) = get_context_info_array($data->contextid);
-        $path = explode('/', $context->path);
-        $topCategoryContextId = $path[3];
-        $topCategoryContext = $DB->get_record_sql("SELECT * FROM {context} WHERE contextlevel=40 AND id=$topCategoryContextId", null, IGNORE_MISSING);
-        $topCategory = $DB->get_record_sql("SELECT * FROM {course_categories} WHERE id=$topCategoryContext->instanceid", null, IGNORE_MISSING);
-    } catch (Exception $e) {
-    }
-
-    echo "<tr>
-        <td><a target='_blank' href=\"https://merchant.cloudpayments.ru/transactions/$payment->TransactionId\">$payment->DateTime</a></td>
-        <td>$payment->Amount</td>
-        <td>$fileName</td>
-        <td><a href=\"/course/view.php?id=$course->id\">$course->fullname</td>
-        <td>$topCategory->name</td>
-        <td>$user->email</td>
-        <td><a href=\"/user/view.php?id=$user->id&course=$course->id\">$user->firstname $user->lastname</a></td>
-    </tr>";
+$paymentsByMonth = [];
+foreach (array_reverse($payments) as $payment) {
+    $date = date_parse($payment->DateTime);
+    $key = $date['year'] . '-' . $date['month'];
+    $paymentsByMonth[$key][] = $payment;
 }
 
-echo "</table>";
+foreach ($paymentsByMonth as $date => $payments) {
+    [$year, $month] = explode('-', $date);
+    $monthName = $monthNames[$month - 1];
+    echo "<h1>$monthName $year </h1>";
+    echo "<table class='generaltable'>
+        <thead>
+            <tr>
+                <th>Время платежа</th>
+                <th>Сумма, руб.</th>
+                <th>Имя файла</th>
+                <th>Курс</th>
+                <th>Подразделение</th>
+                <th>Email</th>
+                <th>Имя</th>
+            </tr>
+        </thead>";
 
-echo "Итого: $total руб.<br>";
-echo "Количество платежей: " . count($payments);
+    $total = 0;
+
+    foreach($payments as $payment) {
+        $data = json_decode($payment->Data);
+
+        $total += (float)$payment->Amount;
+
+        if ($data->contextid === 'ltso') {
+            $fileName = '-';
+            $topCategoryName = 'ЛТСО';
+            $courseText = '-';
+            $userText = '';
+            $userEmail = $payment->AccountId;
+        } else {
+                $file = $DB->get_record_sql("SELECT * FROM {files} WHERE filesize > 0 AND contextid=$data->contextid AND component = 'mod_resource' LIMIT 1", null, IGNORE_MISSING);
+            $fileName = $file ? $file->filename : $data->fileName;
+            $user = $DB->get_record_sql("SELECT * FROM {user} where id = $data->userid", null, IGNORE_MISSING);
+
+            try {
+            list($context, $course, $cm) = get_context_info_array($data->contextid);
+            $path = explode('/', $context->path);
+            $topCategoryContextId = $path[3];
+            $topCategoryContext = $DB->get_record_sql("SELECT * FROM {context} WHERE contextlevel=40 AND id=$topCategoryContextId", null, IGNORE_MISSING);
+            $topCategory = $DB->get_record_sql("SELECT * FROM {course_categories} WHERE id=$topCategoryContext->instanceid", null, IGNORE_MISSING);
+                    $topCategoryName = $topCategory->name;
+                    $userText = "<a href=\"/user/view.php?id=$user->id&course=$course->id\">$user->firstname $user->lastname</a>";
+                    $courseText = "<a href=\"/course/view.php?id=$course->id\">$course->fullname</a>";
+            } catch (Exception $e) {
+            }
+        }
+
+
+
+        echo "<tr>
+            <td><a target='_blank' href=\"https://merchant.cloudpayments.ru/transactions/$payment->TransactionId\">$payment->DateTime</a></td>
+            <td>$payment->Amount</td>
+            <td>$fileName</td>
+            <td>$courseText</td>
+            <td>$topCategoryName</td>
+            <td>$userEmail</td>
+            <td>$userText</td>
+        </tr>";
+    }
+
+    echo "</table>";
+
+    echo "Итого: $total руб.<br>";
+    echo "Количество платежей: " . count($payments) . "<br><br>";
+}
+
 
 echo $OUTPUT->footer();
